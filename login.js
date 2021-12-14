16ab6395541242547446abbeb2d9ec52a195e593
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2021-12-14 22:17:55 +0530

    authentication added

---
+++ login.js
@@ -0,0 +1,68 @@
+document.querySelector("form").addEventListener("submit",function(e){
+    e.preventDefault();
+    login()
+})
+
+let currentUser;
+
+async function login(){
+
+    currentUser = document.getElementById("username").value;
+
+    let loginData = {
+        username:document.getElementById("username").value,
+        password:document.getElementById("password").value
+    }
+
+   loginData = JSON.stringify(loginData);
+
+   let loginApi = "https://masai-api-mocker.herokuapp.com/auth/login";
+
+
+
+  let response = await fetch(loginApi,{
+       method:"POST",
+
+       body:loginData,
+       headers:{
+           "Content-Type":"application/json"
+       }
+   })
+
+let data = await response.json();
+console.log('data:', data)
+let user = document.getElementById("username").value;
+getData(user,data.token)
+}
+
+
+async function getData(username,token){
+console.log(username,token)
+try{
+
+
+    let api = `https://masai-api-mocker.herokuapp.com/user/${username}`;
+
+    let response =await fetch(api,{
+        headers:{
+            "Content-Type": "application/json",
+            
+            Authorization: `Bearer ${token}`,
+        },
+       
+    });
+    
+    let data = await response.json();
+    console.log('data:', data)
+
+    if(currentUser == data.username){
+        alert("matched");
+    }else{
+        alert("not matched");
+    }
+}
+catch(error){
+    console.log('error:', error)
+
+}
+}
