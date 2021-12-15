296bb6445339deaba1229c0d6f077df326966f5b
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2021-12-15 22:34:07 +0530

    product page updated

--- products.js
+++ products.js
@@ -63,53 +63,163 @@ function sortByPrice(){
   }
   
 
-  
-// filter 
-var gender="";
-var manCount = 0;
-var mensProduct = document.getElementById("men");
-mensProduct.addEventListener("click",function(){
-    manCount++;
-    gender = "men";
-    filterbyman(gender);
-});
-var womanCount = 0;
-var womensProduct = document.getElementById("women");
-womensProduct.addEventListener("click",function(){
-    womanCount++;
-    gender = "women";
-    filterbywoman(gender);
-});
+//additional page
 
-function filterbyman(gender){
-    var product = allProducts.filter(item => {
-        if(item.gender== gender){
-           return item;
-        }  
-    })
+let hidenSort = true;
+document.getElementById("categoryButton").addEventListener("click",()=>{
+  if(hidenSort){
+       document.getElementById("dropDownListcategory").style.display="block";
+       hidenSort =false;
+    }
+  else{
+       document.getElementById("dropDownListcategory").style.display="none";
+       hidenSort =true;
+  }
+})
+
+let hidenColor = true;
+document.getElementById("categoryColor").addEventListener("click",()=>{
+    if(hidenColor){
+        document.getElementById("dropDownListcolor").style.display="block";
+        hidenColor = false;
+    }
+    else{
+        document.getElementById("dropDownListcolor").style.display="none";
+        hidenColor = true;
+    }
+ })
+
+
+ let hidenMaterial = true;
+ document.getElementById("categoryMaterial").addEventListener("click",()=>{
+     if(hidenMaterial){
+        document.getElementById("dropDownListmaterial").style.display="block";
+        hidenMaterial = false;
+     }
+     else{
+        document.getElementById("dropDownListmaterial").style.display="none";
+        hidenMaterial = true;
+     }
     
-    if(manCount % 2 == 1){
-        displayItem(product,1);
-    }
-    else{
-        displayItem(allProducts,1); 
-    }
-}
+ 
+ })
 
-function filterbywoman(gender){
-    var product = allProducts.filter(item => {
-        if(item.gender== gender){
-           return item;
-        }  
-    })
-  
-    if(womanCount % 2 == 1){
-        displayItem(product,1);
-    }
-    else{
-        displayItem(allProducts,1); 
-    }
+
+ let hidenPrice = true;
+ document.getElementById("categoryPrice").addEventListener("click",()=>{
+     if(hidenPrice){
+        document.getElementById("dropDownListprice").style.display="block";
+        hidenPrice = false;
+     }
+     else{
+        document.getElementById("dropDownListprice").style.display="none";
+        hidenPrice = true;
+     }
+ 
+ 
+ })
+
+
+ let hidenType = true;
+ document.getElementById("categoryType").addEventListener("click",()=>{
+     if(hidenType){
+        document.getElementById("dropDownListtype").style.display="block";
+        hidenType = false;
+     }
+ else{
+    document.getElementById("dropDownListtype").style.display="none";
+    hidenType = true;
+ }
+ 
+ })
+
+
+ let hidenDesign = true;
+ document.getElementById("categoryDesign").addEventListener("click",()=>{
+     if(hidenDesign){
+        document.getElementById("dropDownListdesign").style.display="block";
+        hidenDesign = false;
+     }
+     else{
+        document.getElementById("dropDownListdesign").style.display="none";
+        hidenDesign = true;
+     }
+    
+ 
+ })
+
+
+ let hidenFabric = true;
+ document.getElementById("categoryFabric").addEventListener("click",()=>{
+     if(hidenFabric){
+        document.getElementById("dropDownListFabric").style.display="block";
+        hidenFabric = false;
+     }
+else{
+    document.getElementById("dropDownListFabric").style.display="none";
+    hidenFabric = true;
 }
+ })
+
+ 
+ document.getElementById("filterButton").addEventListener("click",function(){
+     document.getElementById("additionalPageDiv").style.display="flex";
+  document.getElementById("productPageContainer").style.opacity="0.5";
+
+ });
+
+ document.getElementById("closeAdditionalPage").addEventListener("click",function(){
+    document.getElementById("additionalPageDiv").style.display="none";
+    document.getElementById("productPageContainer").style.opacity="1";
+ })
+ 
+
+// filter 
+// var gender="";
+// var manCount = 0;
+// var mensProduct = document.getElementById("men");
+// mensProduct.addEventListener("click",function(){
+//     manCount++;
+//     gender = "men";
+//     filterbyman(gender);
+// });
+// var womanCount = 0;
+// var womensProduct = document.getElementById("women");
+// womensProduct.addEventListener("click",function(){
+//     womanCount++;
+//     gender = "women";
+//     filterbywoman(gender);
+// });
+
+// function filterbyman(gender){
+//     var product = allProducts.filter(item => {
+//         if(item.gender== gender){
+//            return item;
+//         }  
+//     })
+    
+//     if(manCount % 2 == 1){
+//         displayItem(product,1);
+//     }
+//     else{
+//         displayItem(allProducts,1); 
+//     }
+// }
+
+// function filterbywoman(gender){
+//     var product = allProducts.filter(item => {
+//         if(item.gender== gender){
+//            return item;
+//         }  
+//     })
+  
+//     if(womanCount % 2 == 1){
+//         displayItem(product,1);
+//     }
+//     else{
+//         displayItem(allProducts,1); 
+//     }
+// }
 
 
 
