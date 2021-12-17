e554b81b289a56dcaa3d09033bb5465096cfe503
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2021-12-17 14:14:04 +0530

    cart & footer completed

--- cart.js
+++ cart.js
@@ -3,7 +3,6 @@ let cartData;
 
 //import cart middle & cart right
 import cartMiddle from "./components/cartMiddle.js";
-import cartRight from "./components/cartRight.js";
 
 // cart page display setting
 
@@ -50,14 +49,31 @@ finally{
 
 // show cart products
 
-function displayCartProducts(products){
+async function displayCartProducts(products){
 
+try{
 
+let cartItems = products.length;
+document.getElementById("cartCount").textContent=`(${cartItems} item)`;
+
+// price calculate
+let merchandiseValue = document.getElementById("merchandiseValue");
+let shippingValue = document.getElementById("shippingValue");
+let taxValue = document.getElementById("taxValue");
+let totalEstValue = document.getElementById("totalEstValue");
+
+shippingValue.textContent = "$120.00";
+taxValue.textContent = "$33.00";
+
+let totalItemValue = products.reduce((ac,el)=>{
+    return  ac+el.price
+    },0);
 
 let cartProductContainer = document.getElementById("cartProductContainer");
 cartProductContainer.innerHTML=null;
 
-    products.forEach((element,index) => {
+products.forEach((element,index) => {
+   console.log(element)
 
         //create productContainer 
     let cartProductDetail = document.createElement("div");
@@ -66,6 +82,8 @@ cartProductContainer.innerHTML=null;
     // product top section
     let cartProductTopSection = document.createElement("div");
     cartProductTopSection.setAttribute("id","cartProductTopSection");
+
+
    
     // product top left
     let topLeft = document.createElement("div");
@@ -78,38 +96,54 @@ cartProductContainer.innerHTML=null;
     sku.setAttribute("id","sku");
     let skuRandom = Math.floor(Math.random()*10000)+10000;
     sku.textContent=`sku${skuRandom}`
-    topLeft.append(productName,sku);
-    
 
-    //product top middle
+    topLeft.append(productName,sku);
+
+//     //product top middle
     let topMiddle = document.createElement("div");
     topMiddle.setAttribute("id","topMiddle");
     let quantity = document.createElement("input");
     quantity.setAttribute("id","quantity");
-    quantity.value=1;
+    quantity.defaultValue="1";
+    quantity.setAttribute("type","number");
     let update = document.createElement("button");
+    update.textContent = "update";
     update.setAttribute("id","update");
-    update.textContent="update"
-    topMiddle.append(quantity,update);
-    console.log(quantity,update)
+    update.textContent="update";
 
-    //product top right
+    //count update 
+    let itemQuantity = +quantity.value;
+    quantity.onchange = updateCount;
+
+    function updateCount(){
+        itemQuantity = quantity.value;
+    }
+   
+    merchandiseValue.textContent = `$${((totalItemValue*itemQuantity)).toFixed(2)}`;
+    totalEstValue.textContent = `$${((totalItemValue*itemQuantity)+ 153 ).toFixed(2)}`;
+    update.addEventListener("click",function(){
+        merchandiseValue.textContent = `$${((totalItemValue*itemQuantity)).toFixed(2)}`;
+        document.getElementById("price").textContent = `$${((totalItemValue*itemQuantity)).toFixed(2)}`;
+        totalEstValue.textContent = `$${((totalItemValue*itemQuantity)+ 153 ).toFixed(2)}`;
+    })
+
+    topMiddle.append(quantity,update);
+
+
+        // //product top right
     let topRight = document.createElement("div");
     topRight.setAttribute("id","topRight");
     let price = document.createElement("price");
     price.setAttribute("id","price");
-    price.textContent = element.price;
+    price.textContent = `$${(totalItemValue*itemQuantity)}.00`;
     topRight.append(price);
-    console.log(price);
-
-    cartProductTopSection.append(topLeft,topMiddle,topRight);
 
 
-    //product Bottom section
+    //     //product Bottom section
     let cartProductBottomSection = document.createElement("div");
-    cartProductBottomSection.setAttribute("id",);
+    cartProductBottomSection.setAttribute("id","cartProductBottomSection");
 
-    //product bottom left
+//     //product bottom left
     let bottomLeft = document.createElement("div");
     bottomLeft.setAttribute("id","bottomLeft");
     let productImage = document.createElement("img");
@@ -117,23 +151,82 @@ cartProductContainer.innerHTML=null;
     productImage.src = element.img1;
     bottomLeft.append(productImage);
 
-    // //product bottom middle 
-    // let bottomMiddle = document.createElement("div");
-    // bottomMiddle.setAttribute("id","bottomMiddle");
-    // bottomMiddle = cartMiddle();
+    //product bottom middle 
+    let bottomMiddle = document.createElement("div");
+    bottomMiddle.setAttribute("id","bottomMiddle");
+    bottomMiddle.innerHTML = cartMiddle();
+
     
-    // // product bottom right
-    // let bottomRight = document.createElement("div");
-    // bottomRight.setAttribute("id","bottomRight");
-    // bottomRight = cartRight();
+   // product bottom right
+    let bottomRight = document.createElement("div");
+    bottomRight.setAttribute("id","bottomRight");
+    bottomRight.style.width="200px";
+
+    let cross = document.createElement("div");
+    cross.setAttribute("class","bottomRightDiv");
+    let crossImage = document.createElement("img");
+    crossImage.setAttribute("id","imgCross");
+    crossImage.src = "https://cdn-icons-png.flaticon.com/512/748/748122.png";
+    let crossContent = document.createElement("p");
+    crossContent.textContent="Remove";
+
+    cross.append(crossImage,crossContent);
+
+    cross.addEventListener("click",function(){
+        cartData.splice(index,1);
+        localStorage.setItem("cbWishlistItem",JSON.stringify(cartData));
+        displayCartProducts(cartData);
+    })
+
+    let download = document.createElement("div");
+    download.setAttribute("class","bottomRightDiv");
+    let downloadImage = document.createElement("img");
+    downloadImage.setAttribute("id","imgDownload");
+    downloadImage.src = "https://cdn-icons-png.flaticon.com/512/318/318168.png";
+    let downloadContent = document.createElement("p");
+    downloadContent.textContent="Save for later";
+
+    download.append(downloadImage,downloadContent);
+
+    bottomRight.append(cross,download);
+
+    let hr = document.createElement("hr");
 
 
-    cartProductBottomSection.append(bottomLeft);
-  document.body.append(cartProductTopSection)
-    cartProductDetail.append(cartProductTopSection,cartProductBottomSection);
+    cartProductTopSection.append(topLeft,topMiddle,topRight);
+
+    cartProductBottomSection.append(bottomLeft,bottomMiddle,bottomRight)
+
+    cartProductDetail.append(cartProductTopSection,hr,cartProductBottomSection);
 
     cartProductContainer.append(cartProductDetail);
-    
-    });
+});
+}
+catch(error){
+ console.log('error:', error)
+}
+finally{
+    console.log("cart data appended");
+}
 
 }
+
+
+document.getElementById("applyButton").addEventListener("click",function(){
+  let inputPromo =  document.getElementById("promoCode").value;
+
+  let totalItemValue = cartData.reduce((ac,el)=>{
+    return  ac+el.price
+    },0);
+
+    let itemQuantity = document.getElementById("quantity").value;
+
+   if(inputPromo == "Masai30"){
+    document.getElementById("totalEstValue").textContent = `$${((((totalItemValue*itemQuantity)+ 153) * 70 ) /100).toFixed(2) }`;
+   }
+})
+
+
+document.getElementById("checkOutButton").addEventListener("click",function(){
+    window.location.href = "checkout.html";
+})
